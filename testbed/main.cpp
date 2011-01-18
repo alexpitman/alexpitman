cde413a6204712458ffef42b42c9f2cd0efc9828
alexpitman_webmail@yahoo.com.au <alexpitman@users.noreply.github.com>
2011-01-18 08:37:14 +0000

    Add openGL libraries and cake build scripts

---
+++ testbed/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include <QApplication>
+
+#include "widget/ViewWindow.h"
+
+#include "geometry/Point.h"
+
+int main(int Argc, char** Argv)
+{
+  geo::Point3D p(0, 1, 2);
+
+  std::cout << "Hello Cake!" << std::endl;
+  std::cout << p.X() << " " << p.Y() << " " << p.Z() << std::endl;
+  
+  
+  QApplication app(Argc, Argv);
+  wid::ViewWindow window;
+  
+  window.resize(window.sizeHint());
+  window.show();
+
+  return app.exec();
+}
