f13bf67057f26b3acc26cf64e117a51e772b4532
alexpitman_webmail@yahoo.com.au <alexpitman_webmail@yahoo.com.au@e001a6fc-dcb6-554a-50bb-e39eabf7f74d>
2012-03-24 00:16:32 +0000

    XBox camera controls

--- trunk/common/include/input/XBox.h
+++ trunk/common/include/input/XBox.h
@@ -10,7 +10,9 @@
 
 #include "input/Dll.h"
 
+#include <windows.h>
 #include <XInput.h>
+#include <functional>
 
 namespace in
 {
@@ -32,28 +34,94 @@ namespace in
     XY             = 0x8000,
   };
 
+  enum T_XTrigger
+  {
+    XTriggerLeft,
+    XTriggerRight,
+  };
+  
+  class Dll_in XKey
+  {
+  public:
+    
+    XKey( T_XKey Key );
+    
+    bool Is( T_XKey Key ) const;
+    
+    T_XKey KeyType() const;
+    
+  private:
+  
+    T_XKey myKeyType;
+
+  };
+  
+  typedef std::function<void(const in::XKey& Key)> T_KeyPressCallback;
+  typedef std::function<void(const in::XKey& Key)> T_KeyReleaseCallback;
+  typedef std::function<void(short Value)> T_TriggerCallback;
+  typedef std::function<void(short X, short Y)> T_ThumbCallback;
+  
   class Dll_in XBox
   {
   public:
   
-    XBox(int PlayerNumber);
+    XBox(
+      int PlayerNumber,
+      T_KeyPressCallback KeyPressCallback,
+      T_KeyReleaseCallback KeyReleaseCallback,
+      T_TriggerCallback LeftTriggerCallback,
+      T_TriggerCallback RightTriggerCallback,
+      T_ThumbCallback LeftThumbCallback,
+      T_ThumbCallback RightThumbCallback);
   
-    // Poll the xbox controller
-    bool Update();
-    // Returns false on failure (e.g. connection lost)
+    // Poll the xbox controller for changes.
+    void Poll();
+    // Calls the appropriate callbacks if a button was pressed/released.
   
     // Vibrate the controller
     void Vibrate(int Left, int Right);
     // Controllers have two motors in them (left and right)
     
+    // Check if a button is pressed.
     bool IsPressed( T_XKey Key ) const;
   
+    // Get the trigger pressed down amount.
+    short LeftTrigger() const;
+    short RightTrigger() const;
+    // Range 0-255
+  
+    static const short TriggerMaxValue = 255;
+  
+    // Get the thumb stick position.
+    short ThumbLeftX() const;
+    short ThumbLeftY() const;
+    short ThumbRightX() const;
+    short ThumbRightY() const;
+    // Range -32768 and 32767.
+    
+    static const short ThumbMaxValue = 32767;
+    
   private:
+  
+    // Poll the xbox controller
+    bool Update(XINPUT_STATE* State);
+    // Returns false on failure (e.g. connection lost)
+  
+    // Check if a given key has changed states.
+    bool HasKeyStateChanged( T_XKey Key, XINPUT_STATE Old, XINPUT_STATE New );
   
     int myControllerNumber;
     
+    T_KeyPressCallback myKeyPressCallback;
+    T_KeyReleaseCallback myKeyReleaseCallback;
+    
+    T_TriggerCallback myLeftTriggerCallback;
+    T_TriggerCallback myRightTriggerCallback;
+    
+    T_ThumbCallback myLeftThumbCallback;
+    T_ThumbCallback myRightThumbCallback;
+    
     XINPUT_STATE myState;
-  
   };
 }
 
--- trunk/common/src/input/XBox.cpp
+++ trunk/common/src/input/XBox.cpp
@@ -8,20 +8,127 @@
 
 #include "input/XBox.h"
 
-in::XBox::XBox(int PlayerNumber)
-: myControllerNumber( PlayerNumber - 1 )
+#include <assert.h>
+
+in::XKey::XKey( T_XKey Key )
+: myKeyType(Key)
 {
 }
-
-bool
-in::XBox::Update()
+    
+bool in::XKey::Is( T_XKey Key ) const
 {
-  // Clear the state
-  ZeroMemory(&myState, sizeof(XINPUT_STATE));
+  return myKeyType == Key;
+}
 
-  DWORD sucess = XInputGetState(myControllerNumber, &myState);
+in::T_XKey in::XKey::KeyType() const
+{
+  return myKeyType;
+}
+
+in::XBox::XBox(
+  int PlayerNumber,
+  T_KeyPressCallback KeyPressCallback,
+  T_KeyReleaseCallback KeyReleaseCallback,
+  T_TriggerCallback LeftTriggerCallback,
+  T_TriggerCallback RightTriggerCallback,
+  T_ThumbCallback LeftThumbCallback,
+  T_ThumbCallback RightThumbCallback)
+: myControllerNumber( PlayerNumber - 1 ),
+  myKeyPressCallback(KeyPressCallback),
+  myKeyReleaseCallback(KeyReleaseCallback),
+  myLeftTriggerCallback(LeftTriggerCallback),
+  myRightTriggerCallback(RightTriggerCallback),
+  myLeftThumbCallback(LeftThumbCallback),
+  myRightThumbCallback(RightThumbCallback)
+{
+  ZeroMemory(&myState, sizeof(XINPUT_STATE));
+}
+
+void in::XBox::Poll()
+{
+  XINPUT_STATE oldState = myState;
   
-  return sucess == ERROR_SUCCESS;
+  bool sucess = Update(&myState);
+  // TODO handle controller disconnect etc.
+  
+  // Handle sensitivity issues with the triggers (set to zero if less than threshold).
+  if (myState.Gamepad.bLeftTrigger < XINPUT_GAMEPAD_TRIGGER_THRESHOLD)
+  {
+    myState.Gamepad.bLeftTrigger = 0;
+  }
+  if (myState.Gamepad.bRightTrigger < XINPUT_GAMEPAD_TRIGGER_THRESHOLD)
+  {
+    myState.Gamepad.bRightTrigger = 0;
+  }
+  
+  // Handle sensitivity issues with thumbsticks (set zero if less than threshold).
+  if ( myState.Gamepad.sThumbLX < XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE &&
+       myState.Gamepad.sThumbLX > -XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE &&
+       myState.Gamepad.sThumbLY < XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE &&
+       myState.Gamepad.sThumbLY > -XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE )
+  {
+    myState.Gamepad.sThumbLX = 0;
+    myState.Gamepad.sThumbLY = 0;
+  }
+  if ( myState.Gamepad.sThumbRX < XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE &&
+       myState.Gamepad.sThumbRX > -XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE &&
+       myState.Gamepad.sThumbRY < XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE &&
+       myState.Gamepad.sThumbRY > -XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE )
+  {
+    myState.Gamepad.sThumbRX = 0;
+    myState.Gamepad.sThumbRY = 0;
+  }
+  
+  // Button changes.
+  if (oldState.Gamepad.wButtons != myState.Gamepad.wButtons)
+  {
+    // Check what was pressed/released.
+#define IN_CHECK_BUTTON(BUTTON)                             \
+    if (HasKeyStateChanged(BUTTON, oldState, myState))      \
+    {                                                       \
+      if (IsPressed(BUTTON)) myKeyPressCallback(BUTTON);    \
+      else                   myKeyReleaseCallback(BUTTON);  \
+    }
+    
+    IN_CHECK_BUTTON(XUp);
+    IN_CHECK_BUTTON(XDown);
+    IN_CHECK_BUTTON(XLeft);
+    IN_CHECK_BUTTON(XRight);
+    IN_CHECK_BUTTON(XStart);
+    IN_CHECK_BUTTON(XBack);
+    IN_CHECK_BUTTON(XLeftThumb);
+    IN_CHECK_BUTTON(XRightThumb);
+    IN_CHECK_BUTTON(XLeftShoulder);
+    IN_CHECK_BUTTON(XRightShoulder);
+    IN_CHECK_BUTTON(XA);
+    IN_CHECK_BUTTON(XB);
+    IN_CHECK_BUTTON(XX);
+    IN_CHECK_BUTTON(XY);
+    
+#undef IN_CHECK_BUTTON
+  }
+  
+  // Trigger changes.
+  if (oldState.Gamepad.bLeftTrigger != myState.Gamepad.bLeftTrigger)
+  {
+    myLeftTriggerCallback(myState.Gamepad.bLeftTrigger);
+  }
+  if (oldState.Gamepad.bRightTrigger != myState.Gamepad.bRightTrigger)
+  {
+    myLeftTriggerCallback(myState.Gamepad.bRightTrigger);
+  }
+  
+  // Thumb changes.
+  if (oldState.Gamepad.sThumbLX != myState.Gamepad.sThumbLX ||
+      oldState.Gamepad.sThumbLY != myState.Gamepad.sThumbLY)
+  {
+    myLeftThumbCallback(myState.Gamepad.sThumbLX, myState.Gamepad.sThumbLY);
+  }
+  if (oldState.Gamepad.sThumbRX != myState.Gamepad.sThumbRX ||
+      oldState.Gamepad.sThumbRY != myState.Gamepad.sThumbRY)
+  {
+    myRightThumbCallback(myState.Gamepad.sThumbRX, myState.Gamepad.sThumbRY);
+  }
 }
 
 void
@@ -40,7 +147,77 @@ in::XBox::Vibrate(int Left, int Right)
 }
 
 bool
-in::XBox::IsPressed( T_XKey Key ) const
+in::XBox::IsPressed( in::T_XKey Key ) const
 {
   return myState.Gamepad.wButtons & Key;
+}
+
+short
+in::XBox::LeftTrigger() const
+{
+  return myState.Gamepad.bLeftTrigger;
+}
+
+short
+in::XBox::RightTrigger() const
+{
+  return myState.Gamepad.bRightTrigger;
+}
+  
+short in::XBox::ThumbLeftX() const
+{
+  return myState.Gamepad.sThumbLX;
+}
+
+short
+in::XBox::ThumbLeftY() const
+{
+  return myState.Gamepad.sThumbLY;
+}
+
+short
+in::XBox::ThumbRightX() const
+{
+  return myState.Gamepad.sThumbRX;
+}
+
+short
+in::XBox::ThumbRightY() const
+{
+  return myState.Gamepad.sThumbRY;
+}
+
+bool
+in::XBox::Update(XINPUT_STATE* State)
+{
+  // Clear the state
+  ZeroMemory(State, sizeof(XINPUT_STATE));
+
+  DWORD sucess = XInputGetState(myControllerNumber, State);
+  
+  return sucess == ERROR_SUCCESS;
+}
+
+bool in::XBox::HasKeyStateChanged( in::T_XKey Key, XINPUT_STATE Old, XINPUT_STATE New )
+{
+  switch (Key)
+  {
+  case in::XUp:
+  case in::XDown:
+  case in::XLeft:
+  case in::XRight:
+  case in::XStart:
+  case in::XBack:
+  case in::XLeftThumb:
+  case in::XRightThumb:
+  case in::XLeftShoulder:
+  case in::XRightShoulder:
+  case in::XA:
+  case in::XB:
+  case in::XX:
+  case in::XY:
+    return (Old.Gamepad.wButtons & Key) != (New.Gamepad.wButtons & Key);
+  }
+  assert(false); // Should not be reachable.
+  return false;
 }
